b6d58852c423548d1d164675259f81d1a11d3213
Administrator <869937742@qq.com>
2018-12-25 14:14:30 +0800

    我是/git/javaAgentTest

--- src/main/java/me/test.java
+++ src/main/java/me/test.java
@@ -3,9 +3,6 @@ package me;
 public class test {
 
     public static void main(String args[]){
-       //git/javaAgentTest
-    System.out.println("我是//git/javaAgentTest");
-        //git//test1
-    System.out.println("我是/git/test1");
+    System.out.println("我是/git/javaAgentTest!");
     }
 }
