a4435f91da1ac617a8a6aff27c55708f3c0524d4
zhangguoqiang@idstaff.com <zhangguoqiang@idstaff.com>
2018-12-25 19:09:40 +0800

    删除test1.java文件

--- src/main/java/me/test1.java
+++
@@ -1,8 +0,0 @@
-package me;
-
-public class test1 {
-    public static void main(String args[]){
-        System.out.println("我是/git/javaAgenttest1");
-        System.out.println("我是/git/javaAgenttest2");
-    }
-}
