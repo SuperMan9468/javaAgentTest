1fceee84c1451ea3a34378904bd541c7cce972f9
Administrator <869937742@qq.com>
2018-12-25 15:49:42 +0800

    我是/git/test1/javaAgenttest1 我是/git/test1/javaAgenttest2 我是/git/test1/javaAgenttest3

--- src/main/java/me/test1.java
+++ src/main/java/me/test1.java
@@ -4,5 +4,6 @@ public class test1 {
     public static void main(String args[]){
         System.out.println("我是/git/javaAgenttest2");
         System.out.println("我是/git/javaAgenttest2");
+        System.out.println("我是/git/javaAgenttest3");
     }
 }
